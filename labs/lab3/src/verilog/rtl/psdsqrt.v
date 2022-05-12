91ba510f2f8dfc0b13b20e24954fc0d227b3dea0
JCaldas <joaosantoscaldas@gmail.com>
2022-05-12 14:57:11 +0100

    lab3 downloaded do moodle

--- labs/lab3/src/verilog/rtl/psdsqrt.v
+++ labs/lab3/src/verilog/rtl/psdsqrt.v
@@ -1,32 +1,32 @@
-module psdsqrt(
+module psdsqrt
+      #(parameter NBITSIN = 32) (
         input clock,
 				input reset,
 				input start,
 				input stop,
-        input [5:0] NBITSIN,
-				input [63:0] xin,
-				output reg [31:0] sqrt
+				input [NBITSIN-1:0] xin,
+				output reg [NBITSIN/2-1:0] sqrt
 			  );
 
-wire [31:0] aux;
-wire [31:0] aux2;
-wire [63:0] aux3;
-wire [63:0] Q;
-wire [63:0] sqtestsqrt;
-wire [31:0] testsqrt;
+wire [NBITSIN/2-1:0] aux;
+wire [NBITSIN/2-1:0] aux2;
+wire [NBITSIN/2-1:0] aux3;
+wire [NBITSIN-1:0] Q;
+wire [NBITSIN-1:0] sqtestsqrt;
+wire [NBITSIN/2-1:0] testsqrt;
 
-reg [31:0] leftD;
-reg [31:0] rightD;
+reg [NBITSIN/2-1:0] leftD;
+reg [NBITSIN/2-1:0] rightD;
 
-reg [31:0] tempsqrt;
-reg [31:0] rightQ;
+reg [NBITSIN/2-1:0] tempsqrt;
+reg [NBITSIN/2-1:0] rightQ;
 
 always@(posedge clock)
 begin
 if(reset)
 begin
-	tempsqrt <= 32'h0;
-	rightQ <= 32'h0;
+	tempsqrt <= NBITSIN/2'h0;
+	rightQ <= NBITSIN/2'h0;
 end
 else
 begin
@@ -53,15 +53,15 @@ dff32 out(
 
 assign sqtestsqrt = testsqrt * testsqrt;
 assign testsqrt = tempsqrt | rightQ;
-assign aux2 = 32'h1<<(NBITSIN/2-1);
-assign aux3 = (32'h1<<NBITSIN) -1;
+assign aux2 = NBITSIN/2'h1<<(NBITSIN/2-1);
+assign aux3 = (NBITSIN/2-1'h1<<NBITSIN) -1;
 
 always @*
 begin
 
 
 if (start)
-    leftD = 32'h0;
+    leftD = NBITSIN/2-1'h0;
 else
 begin
     if (Q  >= (sqtestsqrt))
@@ -72,7 +72,7 @@ end
 
 if (start)
 begin
-    rightD <= aux2;
+    rightD = aux2;
 end
 else
     rightD = rightQ >> 1;
